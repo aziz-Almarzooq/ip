868b0a01d0242b630d61eb3423874c02f1fadaee
Aziz Almarzooq <e1375441@u.nus.edu>
2026-08-20 18:15:02 +0800

    Add command echo and bye handling

--- src/main/java/Adrian.java
+++ src/main/java/Adrian.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Adrian {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String banner = "    _       _      _             \n"
                 + "   / \\   __| |_ __(_) __ _ _ __  \n"
                 + "  / _ \\ / _` | '__| |/ _` | '_ \\ \n"
@@ -13,7 +16,18 @@ public class Adrian {
         System.out.println("Hello! I'm Adrian.");
         System.out.println("What can I do for you?");
         System.out.println(line);
+
+        String input = scanner.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(" " + input);
+            System.out.println(line);
+            input = scanner.nextLine();
+
+        }
+
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
+        scanner.close();
     }
 }
