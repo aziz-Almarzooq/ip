07cb9bbd15b7beeb792a130a98cf2088faba0646
Aziz Almarzooq <e1375441@u.nus.edu>
2026-08-20 20:08:02 +0800

    Add todo, deadline, and event task types using inheritance

--- src/main/java/Adrian.java
+++ src/main/java/Adrian.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Adrian {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] box = new String[100];
+        Task[] tasks = new Task[100];;
         int number = 0;
         String banner = "    _       _      _             \n"
                 + "   / \\   __| |_ __(_) __ _ _ __  \n"
@@ -20,21 +20,88 @@ public class Adrian {
         System.out.println(line);
 
         String input = scanner.nextLine();
+
         while(!input.equals("bye")) {
             if(input.equals("list")){
                 System.out.println(line);
                 for(int i = 0; i<number;i++) {
-                    System.out.println(i+1 + ". " + box[i]);
+                    System.out.println(i+1 + ". " + tasks[i]);
                 }
                 System.out.println(line);
             }
-            else {
-                box[number] = input;
-                number++;
-                System.out.println("added: " + input);
-                System.out.println(line);
+
+            else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                Task task = tasks[taskNumber - 1];
+
+                task.markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
             }
-                input = scanner.nextLine();
+            else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                Task task = tasks[taskNumber - 1];
+
+                task.markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + task);
+            }
+            else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+
+                tasks[number] = new Todo(description);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[number]);
+
+                number++;
+
+                System.out.println("Now you have " + number
+                        + " tasks in the list.");
+
+            } else if (input.startsWith("deadline ")) {
+                String details = input.substring(9);
+
+                int byIndex = details.indexOf(" /by ");
+
+                String description = details.substring(0, byIndex);
+                String by = details.substring(byIndex + 5);
+
+                tasks[number] = new Deadline(description, by);
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[number]);
+
+                number++;
+
+                System.out.println("Now you have " + number
+                        + " tasks in the list.");
+
+            } else if (input.startsWith("event ")) {
+                String details = input.substring(6);
+
+                int fromIndex = details.indexOf(" /from ");
+                int toIndex = details.indexOf(" /to ");
+
+                String description = details.substring(0, fromIndex);
+                String from = details.substring(fromIndex + 7, toIndex);
+                String to = details.substring(toIndex + 5);
+
+                tasks[number] = new Event(description, from, to);
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[number]);
+
+                number++;
+
+                System.out.println("Now you have " + number
+                        + " tasks in the list.");
+            }
+
+
+            System.out.println(line);
+            input = scanner.nextLine();
 
         }
 
