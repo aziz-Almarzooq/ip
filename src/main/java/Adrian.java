f03acb5a275116ece5d00023d05779b1d93fe276
Aziz Almarzooq <e1375441@u.nus.edu>
2026-08-20 17:59:41 +0800

    Complete Level-0 greeting and exit behavior

---
+++ src/main/java/Adrian.java
@@ -0,0 +1,19 @@
+public class Adrian {
+    public static void main(String[] args) {
+        String banner = "    _       _      _             \n"
+                + "   / \\   __| |_ __(_) __ _ _ __  \n"
+                + "  / _ \\ / _` | '__| |/ _` | '_ \\ \n"
+                + " / ___ \\ (_| | |  | | (_| | | | |\n"
+                + "/_/   \\_\\__,_|_|  |_|\\__,_|_| |_|\n";
+
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Adrian.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
